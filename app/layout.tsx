fc6b65710719fdb82a076e92f7d56d39b7da8d87
Anirudh Kamath <github@kamath.io>
2025-08-11 10:27:56 -0700

    component refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ViewportHeightInitializer from "./components/ViewportHeightInitializer";
+import ViewportHeightInitializer from "../components/ViewportHeightInitializer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
